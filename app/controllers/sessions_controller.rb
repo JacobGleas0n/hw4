4b9f40950179d44c84588a692cd916fe1b1c26ce
JacobGleas0n <jacob.gleason@kellogg.northwestern.edu>
2022-03-10 17:34:47 +0000

    lot of changes made

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,27 @@ class SessionsController < ApplicationController
   end
 
   def create
+
+    @user = User.where({ email: params["email"] })[0]
+    if @user
+      if BCrypt::Password.new(@user.password) == params["password"]
+        session[:user_id] = @user.id
+        flash[:notice] = "Welcome, #{@user.username}."
+        redirect_to "/places"
+      else
+        flash[:notice] = "Password Incorrect"
+        redirect_to "/sessions/new"
+      end
+    else
+      flash[:notice] = "No User Found"
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Goodbye."
+    redirect_to "/sessions/new"
   end
 end
   
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params["id"])
+  end
+
   def new
     @user = User.new
   end
 
   def create
     @user = User.new(params["user"])
+    @user.password = BCrypt::Password.create(params["user"]["password"])
     @user.save
     redirect_to "/"
   end
